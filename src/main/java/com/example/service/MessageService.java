a30ebf1dd21e35d2f21016fad4fc5afddf258b26
Alan Vuong <alanavuong@gmail.com>
2024-07-31 16:58:41 +0000

    Started implemented of createMessage

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,19 @@
 package com.example.service;
 
+import com.example.repository.MessageRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.entity.Message;
+
 public class MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    public Message createMessage(Message message){
+        final boolean IS_EMPTY_MESSAGE = message.getMessageText().length() == 0;
+        
+        return null;
+    }
 }
